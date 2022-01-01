b69176b2adc58a4b08d90905dc7c5c648b459815
Armand Rathgeb <armand.rathgeb@gmail.com>
2021-12-31 23:03:39 -0600

    Add shellscript

---
+++ fizzbuzz.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# You have to install bc for this to work
+for i in {1..100}
+do
+    if [ $(expr $i % 15) -eq 0 ]; then
+        echo "fizzbuzz"
+    elif [ $(expr $i % 3) -eq 0 ]; then
+        echo "fizz"
+    elif [ $(expr $i % 5) -eq 0 ]; then
+        echo "buzz"
+    else
+        echo "$i"
+    fi
+done
