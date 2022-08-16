dc55e73fffe87b5dfdec5c7689b713a91c99a57c
Armand Rathgeb <arathgeb@artixbtw.localdomain>
2022-08-16 00:38:21 -0500

    Format go and python, change C++ to compile-time

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 
+template<int i, bool div3, bool div4>
+struct Fizzbuzz {
+    constexpr static auto val = i;
+};
+
+template<int i>
+struct Fizzbuzz<i, true, true> {
+    constexpr static auto val = "Fizzbuzz";
+};
+
+template<int i>
+struct Fizzbuzz<i, true, false> {
+    constexpr static auto val = "Fizz";
+};
+
+template<int i>
+struct Fizzbuzz<i, false, true> {
+    constexpr static auto val = "Buzz";
+};
+
+template<int i>
+void out() {
+    out<i - 1>();
+    std::cout << Fizzbuzz<i, i % 3 == 0, i % 5 == 0>::val << '\n';
+}
+
+template<>
+void out<0>() { }
+
 int main() {
-    for(int i = 1; i < 101; ++i) {
-        if(i % 15 == 0) {
-            std::cout << "fizzbuzz";
-        } else if(i % 3 == 0) {
-            std::cout << "fizz";
-        } else if(i % 5 == 0) {
-            std::cout << "buzz";
-        } else {
-            std::cout << i;
-        }
-        std::cout << std::endl;
-    }
+    out<100>();
 }
--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i < 101; i++ {
-		if i%15 == 0 {
-			fmt.Println("fizzbuzz")
-		} else if i%3 == 0 {
-			fmt.Println("fizz")
-		} else if i%5 == 0 {
-			fmt.Println("buzz")
-		} else {
-			fmt.Println(i)
-		}
-	}
+    for i := 1; i < 101; i++ {
+        if i%15 == 0 {
+            fmt.Println("fizzbuzz")
+        } else if i%3 == 0 {
+            fmt.Println("fizz")
+        } else if i%5 == 0 {
+            fmt.Println("buzz")
+        } else {
+            fmt.Println(i)
+        }
+    }
 }
